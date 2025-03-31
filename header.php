e279ea3853822c0f2dcb44f1657cbe2a3ab20ad4
Alexander Agnarson <contact@alx.media>
2025-03-31 13:31:05 +0200

    Add missing esc_url

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 
 <div class="page-wrapper">
 	
-	<img class="page-wrapper-lights" src="<?php echo get_template_directory_uri(); ?>/img/lights.png" />
+	<img class="page-wrapper-lights" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/lights.png" />
 	
 	<header class="header nav-menu-dropdown-left">
 		<div class="header-inner">
