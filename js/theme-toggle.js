6b695560fa65923d8ed00fabe38991314f80e806
Alexander Agnarson <contact@alx.media>
2025-03-31 13:20:37 +0200

    Kirki deprecated fix, theme-toggle prefix, style.css links update

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,12 +1,12 @@
 // Get current theme
-var theme = localStorage.getItem( 'theme' );
+var colordive_theme = localStorage.getItem( 'theme' );
 // Set defaults if theme is not defined.
-if ( ! theme ) {
+if ( ! colordive_theme ) {
   localStorage.setItem( 'theme', 'light' );
-  theme = 'light';
+  colordive_theme = 'light';
 }
 // Add theme to the body.
-document.body.classList.add( theme );
+document.body.classList.add( colordive_theme );
 
 // Handle onClick events
 document.getElementById( 'theme-toggle' ).addEventListener( 'click', () => {
@@ -14,9 +14,9 @@ document.getElementById( 'theme-toggle' ).addEventListener( 'click', () => {
   document.body.classList.remove( 'light' );
   document.body.classList.remove( 'dark' );
   // Change the theme.
-  theme = ( theme === 'light' ) ? 'dark' : 'light';
+  colordive_theme = ( colordive_theme === 'light' ) ? 'dark' : 'light';
   // Save the theme.
-  localStorage.setItem( 'theme', theme );
+  localStorage.setItem( 'theme', colordive_theme );
   // Apply the theme.
-  document.body.classList.add( theme );
+  document.body.classList.add( colordive_theme );
 });
